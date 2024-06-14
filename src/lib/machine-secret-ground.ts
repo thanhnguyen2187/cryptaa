98e3baac7d196e8d7caa27bf6eea0a26215fa1b4
Thanh Nguyen <thanhnguyen2187@gmail.com>
2024-06-15 05:15:14 +0700

    Implement `/secret-ground`

---
+++ src/lib/machine-secret-ground.ts
@@ -0,0 +1,262 @@
+import { assign, fromPromise, setup } from "xstate";
+import { aesGcmDecrypt, aesGcmEncrypt } from "../data/encryption";
+import type { ToastStore } from "@skeletonlabs/skeleton";
+
+export type Mode = "encryption" | "decryption";
+
+const encryptLogic = fromPromise(
+  async ({
+    input,
+  }: { input: { text: string; password: string } }): Promise<string> => {
+    return await aesGcmEncrypt(input.text, input.password);
+  },
+);
+
+const decryptLogic = fromPromise(
+  async ({
+    input,
+  }: { input: { text: string; password: string } }): Promise<string> => {
+    return await aesGcmDecrypt(input.text, input.password);
+  },
+);
+
+export const machine = setup({
+  types: {
+    context: {} as {
+      text: string;
+      password: string;
+      textTarget: string;
+      toastStore: ToastStore;
+      mode: Mode;
+    },
+    events: {} as
+      | { type: "SwapMode" }
+      | { type: "SetText"; value: string }
+      | { type: "SetPassword"; value: string }
+      | { type: "Encrypt" }
+      | { type: "Decrypt" },
+    input: {} as {
+      toastStore: ToastStore;
+    },
+  },
+  actors: {
+    encryptLogic,
+  },
+  guards: {
+    IsTextEmpty: ({ context }) => context.text === "",
+    IsPasswordEmpty: ({ context }) => context.password === "",
+    IsBothEmpty: ({ context }) =>
+      context.text === "" && context.password === "",
+    IsEncrypting: ({ context }) => context.mode === "encryption",
+    IsDecrypting: ({ context }) => context.mode === "decryption",
+    IsWrongPassword: (_, params: { error: Error }) =>
+      params.error.message === "Decrypt failed",
+    IsWrongFormat: (_, params: { error: Error }) =>
+      params.error.message === "Invalid ciphertext",
+  },
+  actions: {},
+}).createMachine({
+  id: "MachineSecretGround",
+  context: ({ input }) => ({
+    text: "",
+    password: "",
+    textTarget: "",
+    toastStore: input.toastStore,
+    mode: "encryption",
+  }),
+  initial: "Idling",
+  on: {},
+  states: {
+    Idling: {
+      on: {
+        Encrypt: {
+          target: "#MachineSecretGround.Inputs",
+        },
+        SwapMode: {
+          actions: assign({
+            mode: ({ context }) =>
+              context.mode === "encryption" ? "decryption" : "encryption",
+            text: ({ context }) => context.textTarget,
+            textTarget: () => "",
+          }),
+        },
+        SetText: {
+          actions: assign({
+            text: ({ event }) => event.value,
+          }),
+        },
+        SetPassword: {
+          actions: assign({
+            password: ({ event }) => event.value,
+          }),
+        },
+      },
+    },
+    Inputs: {
+      initial: "Transient",
+      on: {
+        SetText: {
+          actions: assign({
+            text: ({ event }) => event.value,
+          }),
+        },
+        SetPassword: {
+          actions: assign({
+            password: ({ event }) => event.value,
+          }),
+        },
+      },
+      states: {
+        Transient: {
+          always: [
+            {
+              guard: "IsBothEmpty",
+              target: "BothEmpty",
+            },
+            {
+              guard: "IsTextEmpty",
+              target: "TextEmpty",
+            },
+            {
+              guard: "IsPasswordEmpty",
+              target: "PasswordEmpty",
+            },
+            {
+              target: "TransientEnd",
+            },
+          ],
+        },
+        TextEmpty: {
+          entry: ({ context }) => {
+            context.toastStore.trigger({
+              message: "Text should not be empty!",
+              timeout: 2000,
+            });
+          },
+          always: "TransientEnd",
+        },
+        PasswordEmpty: {
+          entry: ({ context }) => {
+            context.toastStore.trigger({
+              message: "Password should not be empty!",
+              timeout: 2000,
+            });
+          },
+          always: "TransientEnd",
+        },
+        BothEmpty: {
+          entry: ({ context }) => {
+            context.toastStore.trigger({
+              message: "Inputs should not be empty!",
+              timeout: 2000,
+            });
+          },
+          always: "TransientEnd",
+        },
+        TransientEnd: {
+          always: "#MachineSecretGround.Functioning",
+        },
+      },
+    },
+    Functioning: {
+      initial: "Transient",
+      states: {
+        Transient: {
+          always: [
+            {
+              guard: "IsEncrypting",
+              target: "Encrypting",
+            },
+            {
+              target: "Decrypting",
+            },
+          ],
+        },
+        Encrypting: {
+          invoke: {
+            src: encryptLogic,
+            input: ({ context }) => ({
+              text: context.text,
+              password: context.password,
+            }),
+            onDone: {
+              target: "TransientEnd",
+              actions: assign({
+                textTarget: ({ event }) => event.output as string,
+              }),
+            },
+            onError: {
+              target: "EncryptionError",
+            },
+          },
+        },
+        EncryptionError: {},
+        Decrypting: {
+          invoke: {
+            src: decryptLogic,
+            input: ({ context }) => ({
+              text: context.text,
+              password: context.password,
+            }),
+            onDone: {
+              target: "TransientEnd",
+              actions: assign({
+                textTarget: ({ event }) => event.output as string,
+              }),
+            },
+            onError: [
+              {
+                guard: {
+                  type: "IsWrongPassword",
+                  params: ({ event }) => ({ error: event.error as Error }),
+                },
+                target: "DecryptionWrongPassword",
+              },
+              {
+                guard: {
+                  type: "IsWrongFormat",
+                  params: ({ event }) => ({ error: event.error as Error }),
+                },
+                target: "DecryptionWrongText",
+              },
+              {
+                actions: ({ event }) => console.error(event.error),
+                target: "DecryptionError",
+              }
+            ],
+          },
+        },
+        DecryptionWrongPassword: {
+          entry: ({ context }) => {
+            context.toastStore.trigger({
+              message: "Please recheck your password!",
+              timeout: 2000,
+            });
+          },
+          always: "TransientEnd",
+        },
+        DecryptionWrongText: {
+          entry: ({ context }) => {
+            context.toastStore.trigger({
+              message: "Please recheck your text's content or format!",
+              timeout: 2000,
+            });
+          },
+          always: "TransientEnd",
+        },
+        DecryptionError: {
+          entry: ({ context }) => {
+            context.toastStore.trigger({
+              message: "Unknown error happened",
+              timeout: 2000,
+            });
+          },
+          always: "TransientEnd",
+        },
+        TransientEnd: {
+          always: "#MachineSecretGround.Idling",
+        },
+      },
+    },
+  },
+});
---
+++ src/routes/secret-ground/+page.svelte
@@ -0,0 +1,89 @@
+<script lang="ts">
+import { CodeBlock } from "@skeletonlabs/skeleton";
+import { Fa } from "svelte-fa";
+import {
+  faArrowsUpDown,
+  faKey,
+  faLock,
+  faNoteSticky,
+} from "@fortawesome/free-solid-svg-icons";
+import { machine } from "$lib/machine-secret-ground";
+import { useMachine } from "@xstate/svelte";
+import { getToastStore } from "@skeletonlabs/skeleton";
+
+const toastStore = getToastStore();
+
+const { snapshot, send } = useMachine(machine, { input: { toastStore } });
+
+function handleTextChanged(e: Event) {
+  const value = (e.target as HTMLInputElement).value;
+  send({ type: "SetText", value });
+}
+function handlePasswordChanged(e: Event) {
+  const value = (e.target as HTMLInputElement).value;
+  send({ type: "SetPassword", value });
+}
+function encrypt() {
+  send({ type: "Encrypt" });
+}
+function swap() {
+  send({ type: "SwapMode" });
+}
+</script>
+
+<div class="container mt-4 mx-auto flex flex-col justify-center items-center">
+  <span>{JSON.stringify($snapshot.context)}</span>
+  <span>{JSON.stringify($snapshot.value)}</span>
+  <div class="card p-4 w-full sm:w-80 flex flex-col gap-4">
+    <label class="label">
+      {#if $snapshot.context.mode === "encryption"}
+        <span>Text to be encrypted</span>
+      {:else if $snapshot.context.mode === "decryption"}
+        <span>Text to be decrypted</span>
+      {/if}
+      <textarea
+        class="textarea font-mono"
+        rows="8"
+        spellcheck="false"
+        on:change={handleTextChanged}
+      >{$snapshot.context.text}</textarea>
+    </label>
+    <label class="label">
+      <span>Password</span>
+      <input
+        class="input"
+        type="password"
+        on:change={handlePasswordChanged}
+      />
+    </label>
+
+    <label class="label">
+      {#if $snapshot.context.mode === "encryption"}
+        <span>Encrypted text</span>
+      {:else if $snapshot.context.mode === "decryption"}
+        <span>Decrypted text</span>
+      {/if}
+      <textarea
+        class="textarea font-mono"
+        rows="8"
+        spellcheck="false"
+        disabled
+      >{$snapshot.context.textTarget}</textarea>
+    </label>
+    <div class="w-full flex flex-row-reverse gap-2">
+      <button
+        class="btn btn-icon variant-soft-secondary"
+        on:click={encrypt}
+      >
+        <Fa icon={faKey}/>
+      </button>
+      <button
+        class="btn btn-icon variant-soft-secondary"
+        on:click={swap}
+      >
+        <Fa icon={faArrowsUpDown}/>
+      </button>
+    </div>
+
+  </div>
+</div>
