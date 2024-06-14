9dc1ec14c72372bf63c98cb463f09568980437f3
Thanh Nguyen <thanhnguyen2187@gmail.com>
2024-06-14 10:36:57 +0700

    Change `--include` to `--also`

--- flake.nix
+++ flake.nix
@@ -16,7 +16,7 @@
       src = ./.;
       npmDepsHash = "sha256-CqTItezUEQ+YHMCiGVWovqm5ZZIDIQ4+9cWS1W5D0kk=";
       dontNpmBuild = false;
-      npmInstallFlags = "--include=dev";
+      npmInstallFlags = "--also=dev";
     };
   };
 }
