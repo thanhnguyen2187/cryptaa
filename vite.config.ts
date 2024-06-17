59be7b797f0f2b982286ed9d026a914dc5ec2e24
Thanh Nguyen <thanhnguyen2187@gmail.com>
2024-06-17 23:52:46 +0700

    Make the application installable as PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,35 @@
-import { purgeCss } from 'vite-plugin-tailwind-purgecss';
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { purgeCss } from "vite-plugin-tailwind-purgecss";
+import { sveltekit } from "@sveltejs/kit/vite";
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
+import { SvelteKitPWA } from '@vite-pwa/sveltekit';
 
 export default defineConfig({
-	plugins: [sveltekit(), purgeCss()]
+  plugins: [
+    sveltekit(),
+    purgeCss(),
+    SvelteKitPWA({
+      registerType: "autoUpdate",
+      devOptions: { enabled: true },
+      manifest: {
+        name: "Cryptaa",
+        short_name: "cryptaa",
+        description: "Offline-first note-taking app",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+      strategies: "generateSW",
+    }),
+  ],
 });
